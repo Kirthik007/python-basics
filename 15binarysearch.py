e0fde82476489bfe16c0b18b0bf446276edb4f92
Kirthik007 <115078968+Kirthik007@users.noreply.github.com>
2025-01-03 19:31:31 +0530

    binary search algorithm for array searching target value given

---
+++ 15binarysearch.py
@@ -0,0 +1,21 @@
+''' Binary search is a simple search algo to find target in an list/arr
+    first array must be sorted in ascending
+    then followed algo below
+
+'''
+def binarysearch(arr,t):
+    
+    l = 0
+    r = len(a)-1
+    while l <= r:
+        m = (l+r)//2
+        if arr[m] == t:
+            return m
+        elif l < arr[m]:
+            l = m+1
+        else:
+            h = m-1
+    return -1
+a = [1,2,4,5,6,7,8,9,33,44,56,77]
+r = binarysearch(a,33)
+print(f'the index of target is {r}  no: {a[r]}')
