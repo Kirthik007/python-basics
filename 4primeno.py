63c09e96d8a778f8f356f3702f40c0526ae57448
Kirthik007 <115078968+Kirthik007@users.noreply.github.com>
2025-02-04 15:21:22 +0530

    prime simplified

--- 4primeno.py
+++ 4primeno.py
@@ -1,13 +1,16 @@
 """ find prime no or not """
 
 n = int(input("enter no "))
-if n >1:
+if n < 2:
+        print("Not prime ")
+else:
         for i in range(2,n):
             if n%i == 0:
                 print("not prime")
                 break
         else:
-                print("prime")
-else:
-    print("Not prime ")
+            print("prime")
+            
+
+    
     
