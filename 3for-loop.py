00a79fec055a6b5cb685a63de308b4dcd5b62b76
Kirthik007 <115078968+Kirthik007@users.noreply.github.com>
2025-03-04 12:34:43 +0530

    nothing

--- 3for-loop.py
+++ 3for-loop.py
@@ -8,7 +8,7 @@ With the for loop we can execute a set of statements, once for each item in a li
 
 """
 
-for x in range(1,20,2):
+for x in range(1,22,2):
     print("char \n" ,x)
 
 adj =["a","b","c"]
